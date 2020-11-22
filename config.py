3b460e02ab7c478c8c9bed7a05a89998f552759f
Sławomir Brzozowski <brzozasr@gmail.com>
2020-11-22 16:01:59 +0100

    Config changes

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import urandom
 
 """Data to login as postgres administrator."""
-# PG_DB = 'ddcars726gde3l'
-# PG_USERNAME = 'vwuafnbhaxvliy'
-# PG_PASSWORD = '78ce242ef9e6dddf4a498ce40b81737dd3182a22d5fda9ed1dd3fb36dd9bfb5e'
+PG_DB = 'ddcars726gde3l'
+PG_USERNAME = 'vwuafnbhaxvliy'
+PG_PASSWORD = '78ce242ef9e6dddf4a498ce40b81737dd3182a22d5fda9ed1dd3fb36dd9bfb5e'
 
 """Host and port setting for connection to PostgreSQL."""
-# PG_HOST = 'ec2-54-246-67-245.eu-west-1.compute.amazonaws.com'
-# PG_PORT = '5432'
+PG_HOST = 'ec2-54-246-67-245.eu-west-1.compute.amazonaws.com'
+PG_PORT = '5432'
 
 """Session variables"""
 SESSION_SECRET_KEY = urandom(64)
@@ -15,8 +15,8 @@ SESSION_USER_ID = 'users_id'
 SESSION_USER_LOGIN = 'users_login'
 
 """Local DB"""
-PG_DB = 'pro_man'
-PG_USERNAME = 'postgres'
-PG_PASSWORD = 'brzozasr'
-PG_HOST = 'localhost'
-PG_PORT = '5432'
+# PG_DB = 'pro_man'
+# PG_USERNAME = 'postgres'
+# PG_PASSWORD = 'brzozasr'
+# PG_HOST = 'localhost'
+# PG_PORT = '5432'
